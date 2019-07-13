81ca32fabc417eda398ba5c69ba16b5d1133e162
Haili Copas-Starke <hcopasstarke@gmail.com>
2019-07-12 17:15:18 -0700

    re-installed everything

--- starwars/src/components/StarCharacters.js
+++ starwars/src/components/StarCharacters.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import './StarWars.css';
+import React from "react"
+import "./StarWars.css"
 
-import { Card } from 'semantic-ui-react';
+import { Card } from "semantic-ui-react"
 
 export const StarCharacters = props => {
 	console.log(props.character.url)
 	return (
 		<div>
 			<br />
-			<Card color="blue" className="card" style={{ margin: "auto", opacity: 0.5 }}>
-
+			<Card color='white' className='card' style={{ margin: "auto", opacity: 0.9 }}>
 				<Card.Content>
-					<Card.Header className="font">{props.character.name}</Card.Header>
-					< Card.Meta> Age: {props.character.birth_year} </Card.Meta>
+					<Card.Header className='font'>{props.character.name}</Card.Header>
+					<Card.Meta> Age: {props.character.birth_year} </Card.Meta>
 					<Card.Description> Gender: {props.character.gender} </Card.Description>
 				</Card.Content>
 			</Card>
